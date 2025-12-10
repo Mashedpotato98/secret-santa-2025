7cfb07e6de3f468787e1bd90819fcf3bd2f12ca5
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2025-12-10 14:32:41 +0600

    projectile

--- santa-2025/player_type/big_guy/weapon/states/gunShoot.gd
+++ santa-2025/player_type/big_guy/weapon/states/gunShoot.gd
@@ -1,8 +1,18 @@
 class_name gunShoot extends State
 
+@export var muzzle:Marker2D
+@export var bullet_load:PackedScene
+
 func Enter():
 	animation_player.play('shoot')
+	_instance_bullet()
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == 'shoot':
 		Transitioned.emit(self, 'gunIdle')
+
+func _instance_bullet():
+	var bullet:CharacterBody2D = bullet_load.instantiate()
+	bullet.global_position = muzzle.global_position
+	bullet.direction = Vector2.from_angle(owner_node.rotation)
+	get_tree().root.add_child(bullet)
