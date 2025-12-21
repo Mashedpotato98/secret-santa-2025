fb2992005998b95296949a96d8cfca551a2d241c
COMRADECHOnKy <129215115+Mashedpotato98@users.noreply.github.com>
2025-12-21 10:17:05 +0600

    let us see if it works

--- santa-2025/enemies/tank_melee/states/melee_stun.gd
+++ santa-2025/enemies/tank_melee/states/melee_stun.gd
@@ -6,6 +6,9 @@ func Enter():
 	if hurtbox.hp <= 0:
 		owner_node.died.emit(owner_node)
 		queue_free()
+	else :
+		if !animation_player.is_playing():
+			animation_player.play('hit')
 	await get_tree().create_timer(1.0).timeout
 	Transitioned.emit(self, 'meleeChase')
 	
